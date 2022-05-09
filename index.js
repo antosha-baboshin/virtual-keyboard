dd5b7a474760ac92c65dbce7b4a254b3a40829d2
Antosha Baboshin <antosha.js@gmail.com>
2022-05-09 15:27:36 +0300

    feat: add capslock

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ let header = document.createElement('h1');
 let textarea = document.createElement('textarea');
 let div = document.createElement('div');
 let span = document.createElement('span');
+let isCaps = false;
 
 document.body.append(header);
 document.body.append(textarea);
@@ -13,9 +14,12 @@ document.body.append(div);
 document.body.append(span);
 
 header.innerHTML = 'Virtual Keyboard';
-span.innerHTML = 'The keyboard is created for Windows'
+span.innerHTML = 'The keyboard is created for Windows<br>Press SHIFT + CTRL to change language'
 div.setAttribute('id', 'keyboard');
-createKeyboard();
+
+const DEFAULT_LANGUAGE = 'en';
+let language = localStorage.getItem('language') || DEFAULT_LANGUAGE;
+createKeyboard(language);
 
 /* function addCollection() {
     let i = 0;
@@ -27,10 +31,22 @@ createKeyboard();
 };
 addCollection(); */
 
-function createKeyboard() {
+function createKeyboard(language) {
     let symb = '';
     for (let key in KEYBOARD) {
-        symb += `<div class='key' data='${key}'>${KEYBOARD[key].en}</div>`;
+        if (language == 'en') {
+            if (isCaps) {
+                symb += `<div class='key' data='${key}'>${KEYBOARD[key].EN}</div>`;    
+            } else {
+                symb += `<div class='key' data='${key}'>${KEYBOARD[key].en}</div>`;
+            }
+        } else {
+            if (isCaps) {
+                symb += `<div class='key' data='${key}'>${KEYBOARD[key].RU}</div>`;
+            } else {
+                symb += `<div class='key' data='${key}'>${KEYBOARD[key].ru}</div>`;
+            }
+        }
     }
     document.querySelector('#keyboard').innerHTML = symb;
 };
@@ -39,16 +55,19 @@ document.onkeydown = function(event) {
     let active_key = document.querySelector(`.key[data='${event.code}']`);
     active_key.classList.add('active');
     writeText(event);
+    shiftKeys(event);
+    changeLanguage(event);
 }
 document.onkeyup = function(event) {
     let active_key = document.querySelector(`.key[data='${event.code}']`);
     active_key.classList.remove('active');
+    unshiftKeys(event);
 }
 
-const KEYS = document.querySelectorAll('.key');
+let keys = document.querySelectorAll('.key');
 
-KEYS.forEach((key) => key.addEventListener('mousedown', addActiveOnClick));
-KEYS.forEach((key) => key.addEventListener('mouseup', removeActiveOnClick));
+keys.forEach((key) => key.addEventListener('mousedown', addActiveOnClick));
+keys.forEach((key) => key.addEventListener('mouseup', removeActiveOnClick));
 
 function addActiveOnClick() {
     event.target.classList.add('active');
@@ -57,15 +76,129 @@ function removeActiveOnClick() {
     event.target.classList.remove('active');
 }
 
-KEYS.forEach((key) => key.addEventListener('mousedown', addActiveOnClick));
+keys.forEach((key) => key.addEventListener('mousedown', addActiveOnClick));
 
 function writeText(event) {
     if (event.code == 'Backspace') {
         textarea.innerHTML = textarea.innerHTML.slice(0, -1);
-    } else if (event.code == 'Tab' || event.code == 'Delete' || event.code == 'CapsLock' || event.code == 'Enter' || event.code == 'ShiftLeft' || event.code == 'ArrowUp' || event.code == 'ShiftRight' || event.code == 'ControlLeft' || event.code == 'MetaLeft' || event.code == 'AltLeft' || event.code == 'AltLeft' || event.code == 'AltRight' || event.code == 'ArrowLeft' || event.code == 'ArrowDown' || event.code == 'ArrowRight' || event.code == 'ControlRight') {
+    } else if (event.code == 'Tab' || event.code == 'Delete' || event.code == 'Enter' || event.code == 'ShiftLeft' || event.code == 'ShiftRight' || event.code == 'ControlLeft' || event.code == 'MetaLeft' || event.code == 'AltLeft' || event.code == 'AltLeft' || event.code == 'AltRight' || event.code == 'ControlRight') {
         textarea.innerHTML = textarea.innerHTML;
+    } else if (event.code === 'CapsLock') {
+        capsKeys();
     } else {
-        textarea.innerHTML += KEYBOARD[event.code].en;
+        let current_key = document.querySelector(`.key[data='${event.code}']`);
+        textarea.innerHTML += current_key.innerHTML;
     }
 }
 
+function shiftKeys(event) {
+    keys = document.querySelectorAll('.key');
+    if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
+        if (language === 'en') {
+            if (q_key.innerHTML === 'q') {
+                keys.forEach((key) => { 
+                    key.innerHTML = KEYBOARD[key.getAttribute('data')].EN; 
+                });
+            } else {
+                keys.forEach((key) => { 
+                    key.innerHTML = KEYBOARD[key.getAttribute('data')].en; 
+                });
+            }
+        } else {
+            if (q_key.innerHTML === 'й') {
+                keys.forEach((key) => { 
+                    key.innerHTML = KEYBOARD[key.getAttribute('data')].RU; 
+                });
+            } else {
+                keys.forEach((key) => { 
+                    key.innerHTML = KEYBOARD[key.getAttribute('data')].ru; 
+                });
+            }
+        }
+    }
+}
+
+function unshiftKeys(event) {
+    if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
+        if (language === 'en') {
+            if (q_key.innerHTML === 'q') {
+                keys.forEach((key) => { 
+                    key.innerHTML = KEYBOARD[key.getAttribute('data')].EN; 
+                });
+            } else {
+                keys.forEach((key) => { 
+                    key.innerHTML = KEYBOARD[key.getAttribute('data')].en; 
+                });
+            }
+        } else {
+            if (q_key.innerHTML === 'й') {
+                keys.forEach((key) => { 
+                    key.innerHTML = KEYBOARD[key.getAttribute('data')].RU; 
+                });
+            } else {
+                keys.forEach((key) => { 
+                    key.innerHTML = KEYBOARD[key.getAttribute('data')].ru; 
+                });
+            }
+        }
+    }
+}
+
+function changeLanguage(event) {
+    const LEFT_CONTROL = document.querySelector('.key[data="ControlLeft"]');
+    const RIGHT_CONTROL = document.querySelector('.key[data="ControlRight"]');
+    const LEFT_SHIFT = document.querySelector('.key[data="ShiftLeft"]');
+    const RIGHT_SHIFT = document.querySelector('.key[data="ShiftRight"]');
+
+    if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
+        if (LEFT_CONTROL.classList.contains('active') || RIGHT_CONTROL.classList.contains('active')) {
+            if (language === 'ru') {
+                language = 'en';
+            } else {
+                language = 'ru';
+            }
+            createKeyboard(language);
+        }
+    } else if (event.code === 'ControlLeft' || event.code === 'ControlRight') {
+        if (LEFT_SHIFT.classList.contains('active') || RIGHT_SHIFT.classList.contains('active')) {
+            if (language === 'ru') {
+                language = 'en';
+            } else {
+                language = 'ru';
+            }
+            createKeyboard(language);
+        }
+    }
+    localStorage.setItem('language', language);
+}
+
+let q_key = document.querySelector('.key[data="KeyQ"]');
+
+function capsKeys() {
+    keys = document.querySelectorAll('.key');
+    if (language === 'en') {
+        if (!isCaps) {
+            keys.forEach((key) => { 
+                key.innerHTML = KEYBOARD[key.getAttribute('data')].EN; 
+            });
+            isCaps = true;
+        } else {
+            keys.forEach((key) => { 
+                key.innerHTML = KEYBOARD[key.getAttribute('data')].en; 
+            });
+            isCaps = false;
+        }
+    } else {
+        if (!isCaps) {
+            keys.forEach((key) => { 
+                key.innerHTML = KEYBOARD[key.getAttribute('data')].RU; 
+            });
+            isCaps = true;
+        } else {
+            keys.forEach((key) => { 
+                key.innerHTML = KEYBOARD[key.getAttribute('data')].ru; 
+            });
+            isCaps = false;
+        }
+    }
+}
